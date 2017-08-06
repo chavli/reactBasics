21c813a1144ea496564dd5b71120dc93572adbd3
Cha Li <chavli@gmail.com>
2017-08-06 15:29:31 -0700

    moving goalcoach to it's own repo

--- goalcoach/src/constants.js
+++
@@ -1,5 +0,0 @@
-export const SIGNED_IN = "SIGNED_IN"
-export const SET_USERS = "SET_USERS"
-export const SET_GOALS = "SET_GOALS"
-export const SET_USER_GOALS = "SET_USER_GOALS"
-export const SET_COMPLETE_GOALS = "SET_COMPLETE_GOALS"
